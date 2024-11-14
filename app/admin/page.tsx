6446f4a44a238287fa50fc832a6fca0e86899bef
ijul-vers <zammuizul@gmail.com>
2024-11-14 21:41:02 +0700

    tambah login

---
+++ app/admin/page.tsx
@@ -0,0 +1,19 @@
+"use client";
+import Breadcrumb from "@/components/Common/Breadcrumb";
+import React from "react";
+
+const AdminPage = () => {
+  return (
+    <>
+      <Breadcrumb
+        pageName="Contact Page"
+        description="Lorem ipsum dolor sit amet, consectetur adipiscing elit. In varius eros eget sapien consectetur ultrices. Ut quis dapibus libero."
+      />
+      <div className="container">
+        <a href="/">admin page mamank</a>
+      </div>
+    </>
+  );
+};
+
+export default AdminPage;
