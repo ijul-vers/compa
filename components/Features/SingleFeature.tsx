e447e9a46c7441c7b6174e4cb27a4da88e7bb1bc
wahyuprihatmoko788 <wahyuprihatmoko788@gmail.com>
2024-11-18 00:52:16 +0000

    p

--- components/Features/SingleFeature.tsx
+++ components/Features/SingleFeature.tsx
@@ -1,3 +1,26 @@
+// import { Feature } from "@/types/feature";
+
+// const SingleFeature = ({ feature }: { feature: Feature }) => {
+//   const { img, nama, paragraph } = feature;
+
+//   return (
+//     <div className="mb-12 "> {/* Menggunakan mb-12 */}
+//       <div className="flex h-21 w-21 items-center justify-center rounded-md bg-primary bg-opacity-10 text-primary"> {/* Menggunakan h-12 w-12 */}
+//         {img} 
+//       </div>
+//       <div className="ml-3"> {/* Bungkus nama dan paragraph, tambahkan margin kiri */}
+//         <h3 className="text-base font-bold text-black dark:text-white sm:text-lg lg:text-base xl:text-lg">
+//           {nama}
+//         </h3>
+//         <p className="text-sm text-body-color"> 
+//           {paragraph}
+//         </p>
+//       </div>
+//     </div>
+//   );
+// };
+// export default SingleFeature;
+
 import { Feature } from "@/types/feature";
 
 const SingleFeature = ({ feature }: { feature: Feature }) => {
@@ -5,7 +28,7 @@ const SingleFeature = ({ feature }: { feature: Feature }) => {
 
   return (
     <div className="mb-12 "> {/* Menggunakan mb-12 */}
-      <div className="flex h-21 w-21 items-center justify-center rounded-md bg-primary bg-opacity-10 text-primary"> {/* Menggunakan h-12 w-12 */}
+      <div className="rounded-3xl overflow-hidden"> {/* Tambahkan div pembungkus dengan rounded-full dan overflow-hidden */}
         {img} 
       </div>
       <div className="ml-3"> {/* Bungkus nama dan paragraph, tambahkan margin kiri */}
@@ -20,4 +43,3 @@ const SingleFeature = ({ feature }: { feature: Feature }) => {
   );
 };
 export default SingleFeature;
-
